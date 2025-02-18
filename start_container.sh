53499546fba2746aeb96df2b7ab9b91cd8681115
Tarun Angalakuduru <91077827+TarunAngalakuduru@users.noreply.github.com>
2025-02-18 17:10:02 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 set -e
 
+# Switch to root user
 sudo su - root
 
-# Pull the Docker image from Docker Hub
-docker pull techwithalok/codepipeline-demo
+# Define AWS Region and ECR Repository details
+REGION="ap-southeast-1"  # Change this as needed
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+ECR_REPO_NAME="my-ecr"  # Change this to your ECR repository name
+IMAGE_TAG="latest"  # Change this tag if needed
+
+# Construct the ECR image URI
+IMAGE_URI="$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPO_NAME:$IMAGE_TAG"
+
+# Authenticate Docker with ECR
+aws ecr get-login-password --region "$REGION" | docker login --username AWS --password-stdin "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com"
+
+# Pull the Docker image from Amazon ECR
+docker pull "$IMAGE_URI"
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 techwithalok/codepipeline-demo
+docker run -d -p 5000:5000 "$IMAGE_URI"
